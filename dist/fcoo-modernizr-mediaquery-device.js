9b2efa9577d919309d192ef234bbf4086289f1dc
Niels Holt <niels@steenbuchholt.dk>
2018-06-04 09:43:41 +0200

    Using fcoo/modernizr-mediaquery 1.3 and setting breakpoints as options to allow async loading of .js and .css

--- dist/fcoo-modernizr-mediaquery-device.js
+++ dist/fcoo-modernizr-mediaquery-device.js
@@ -15,6 +15,22 @@
     window.fcoo = window.fcoo || {};
     var ns = window.fcoo;
 
+    //modernizrMediaqueryOptions *MUST* match $html-font-size, $create-FIRST-up, $create-LAST-down and $breakpoints in src/_fcoo-modernizr-mediaquery-variables.scss
+    var modernizrMediaqueryOptions = {
+            useWindowClientDim: true,
+            htmlFontSize      : 16,     // = $html-font-size
+            createFIRSTup     : false,  // = $create-FIRST-up
+            createLASTdown    : false,  // = $create-LAST-down
+            breakpoints       : {       // = $breakpoints
+                "mini"       :    0, //Smaller than small phone portrait (< 320)
+                "xsmall-port":  320, //Small phone portrait (320-479)
+                "xsmall-land":  480, //Small phone landscape (480-576)
+                "small"      :  576, //Small devices
+                "medium"     :  768, //Tablets portrait
+                "large"      :  992, //Table landscape + desttop
+                "xlarge"     : 1200  //Large desttop
+            }
+        };
 
     /******************************************
     Initialize/ready
@@ -32,7 +48,7 @@
         });
 
         //Create fcoo.modernizrMediaquery
-        ns.modernizrMediaquery = new window.ModernizrMediaquery();
+        ns.modernizrMediaquery = new window.ModernizrMediaquery( modernizrMediaqueryOptions );
 
         //For consistency: 'create' modernizr in window.fcoo
         ns.modernizr = Modernizr;
